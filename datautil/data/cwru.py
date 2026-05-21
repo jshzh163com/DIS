fff45be3e5035c66ccac2109904b56f05ddd7879
Zehui Hua <49631403+jshzh163com@users.noreply.github.com>
2026-05-21 19:10:35 -0400

    remove position label from dataset format

--- datautil/data/cwru.py
+++ datautil/data/cwru.py
@@ -12,22 +12,15 @@ class DataList(mydataset):
         self.dataset = dataset
         self.task = 'cwru'
 
-        x, cy, py, sy = loaddata_from_numpy(self.dataset, self.task, root_dir)
+        x, cy, py = loaddata_from_numpy(self.dataset, self.task, root_dir)
         self.people_group = people_group
-        self.position = np.sort(np.unique(sy))
-        self.comb_position(x, cy, py, sy)
+        self.comb_position(x, cy, py)
         self.x = torch.tensor(self.x[:, np.newaxis, :]).float()
 
-    def comb_position(self, x, cy, py, sy):
+    def comb_position(self, x, cy, py):
         for i, peo in enumerate(self.people_group):
             index = np.where(py == peo)[0]
-            tx, tcy, tsy = x[index], cy[index], sy[index]
-            for j, sen in enumerate(self.position):
-                index = np.where(tsy == sen)[0]
-                if j == 0:
-                    ttx, ttcy = tx[index], tcy[index]
-                else:
-                    ttx = np.hstack((ttx, tx[index]))
+            ttx, ttcy = x[index], cy[index]
             if i == 0:
                 self.x, self.labels = ttx, ttcy
             else:
