a6fe07f709c45f1e71ca86963c7fc7a70be4189d
Zehui Hua <49631403+jshzh163com@users.noreply.github.com>
2026-05-21 19:01:23 -0400

    add project files

---
+++ alg/alg.py
@@ -0,0 +1,12 @@
+from alg.algs.DIS import DIS
+
+ALGORITHMS = [
+    'DIS',
+]
+
+
+def get_algorithm_class(algorithm_name):
+    if algorithm_name not in globals():
+        raise NotImplementedError(
+            "Algorithm not found: {}".format(algorithm_name))
+    return globals()[algorithm_name]
