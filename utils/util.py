2b6a85a175ec83ee04d73d155822583511a67ff1
Zehui Hua <49631403+jshzh163com@users.noreply.github.com>
2026-05-21 19:01:17 -0400

    add project files

---
+++ utils/util.py
@@ -0,0 +1,78 @@
+import random
+import numpy as np
+import torch
+import sys
+import os
+
+
+def set_random_seed(seed=0):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def save_checkpoint(filename, alg, args):
+    save_dict = {
+        "args": vars(args),
+        "model_dict": alg.cpu().state_dict()
+    }
+    torch.save(save_dict, os.path.join(args.output, filename))
+
+
+def train_valid_target_eval_names(args):
+    eval_name_dict = {'train': [], 'valid': [], 'target': []}
+    t = 0
+    for i in range(args.domain_num):
+        if i not in args.test_envs:
+            eval_name_dict['train'].append(t)
+            t += 1
+    for i in range(args.domain_num):
+        if i not in args.test_envs:
+            eval_name_dict['valid'].append(t)
+        else:
+            eval_name_dict['target'].append(t)
+        t += 1
+    return eval_name_dict
+
+
+def alg_loss_dict(args):
+    loss_dict = {
+        'DIS': ['cls', 'kd', 'd_cls', 'd_adv', 'cls_adv', 'dissimilarity', 'total'],
+    }
+    return loss_dict[args.algorithm]
+
+
+def print_args(args, print_list):
+    s = "==========================================\n"
+    l = len(print_list)
+    for arg, content in args.__dict__.items():
+        if l == 0 or arg in print_list:
+            s += "{}:{}\n".format(arg, content)
+    return s
+
+
+def print_environ():
+    print("Environment:")
+    print("\tPython: {}".format(sys.version.split(" ")[0]))
+    print("\tPyTorch: {}".format(torch.__version__))
+    print("\tCUDA: {}".format(torch.version.cuda))
+    print("\tCUDNN: {}".format(torch.backends.cudnn.version()))
+    print("\tNumPy: {}".format(np.__version__))
+
+
+class Tee:
+    def __init__(self, fname, mode="a"):
+        self.stdout = sys.stdout
+        self.file = open(fname, mode)
+
+    def write(self, message):
+        self.stdout.write(message)
+        self.file.write(message)
+        self.flush()
+
+    def flush(self):
+        self.stdout.flush()
+        self.file.flush()
